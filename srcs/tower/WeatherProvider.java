a46022961787ed55c6ecd708d787f03340f0f2f5
micas2002 <miguelbernardinosousa2002@gmail.com>
2025-04-15 15:44:42 +0100

    WeatherProvider class is now singleton

--- srcs/tower/WeatherProvider.java
+++ srcs/tower/WeatherProvider.java
@@ -2,8 +2,17 @@ package weatherprovider;
 
 // Singleton
 public class WeatherProvider {
+	private static WeatherProvider instance = null;
 	private String[]	weather;
 
 	private WeatherProvider() {};
+
+	public static WeatherProvider	getInstance() {
+		if (instance == null)
+			instance = new WeatherProvider();
+		
+		return (instance);
+	}
+	
 	public String	getCurrentWeater(Coordiates p_coordiates) {};
 }
