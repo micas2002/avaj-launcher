5de40beac3c822a67e2bff4e48ce097b33bd997b
micas2002 <miguelbernardinosousa2002@gmail.com>
2025-04-17 16:54:26 +0100

    initial file check complete

--- srcs/Main.java
+++ srcs/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-	private static int			nSimRun = 0;
-	private static ArrayList	aircrafts = null;
+	private static int					nSimRun = 0;
+	private static ArrayList<String[]>	aircrafts = new ArrayList<String[]>();
 
 	public static void checkInput(String input) throws Exception {
 		File file = new File(input);
@@ -19,9 +19,12 @@ public class Main {
 		while (myReader.hasNextLine())
 			aircrafts.add(myReader.nextLine().split(" "));
 
-		for(int i = 0; i < aircrafts.size(); i++) {   
-			System.out.print(aircrafts.get(i));
-		}  
+		for (int i = 0; i != aircrafts.size(); ++i) {
+			if (aircrafts.get(i).length != 5)
+				throw new Exception("Invalid aircraft settings");
+			if (aircrafts.get(i)[0].matches("Baloon|JetPlane|Helicopter") == false)
+				throw new Exception(aircrafts.get(i)[0]);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -30,9 +33,9 @@ public class Main {
 				throw new Exception("Wrong number of arguments");
 
 			checkInput(args[0]);
-
-		} catch (Exception e) {
-
+		}
+		catch (Exception e) {
+			System.out.print(e);
 		}
 	}
 }
